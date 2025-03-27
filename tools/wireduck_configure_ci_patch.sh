20386b2ff66dccd214d5347c45ca051b488fefa0
hyehudai <h.yehudai@gmail.com>
2025-03-28 00:39:14 +0200

    tst

--- tools/wireduck_configure_ci_patch.sh
+++ tools/wireduck_configure_ci_patch.sh
@@ -9,11 +9,11 @@ TSHARK_DIR="$PWD/tools/tshark"
 # Create target dir
 mkdir -p "$TSHARK_DIR"
 cd "$TSHARK_DIR"
-
+pwd
 # Download and extract
 wget -N "$TSHARK_URL"
 dpkg -x "$TSHARK_DEB" .
- 
+$TSHARK_DIR/usr/bin/tshark -v
 
 # Add alias to your shell temporarily (for current session)
 #echo "alias tshark='$TSHARK_DIR/bin/tshark'"
