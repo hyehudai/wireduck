4f0ed2bc39a3f5cad784216f271732870e55b278
hyehudai <h.yehudai@gmail.com>
2025-03-27 23:42:10 +0200

    sudo

--- tools/wireduck_configure_ci_patch.sh
+++ tools/wireduck_configure_ci_patch.sh
@@ -3,7 +3,8 @@ set -euxo pipefail
 
 if ! command -v tshark > /dev/null; then
     echo "Installing tshark inside Docker build..."
-    apt-get update -y && apt-get install -y tshark
+    sudo apt-get update -y
+    sudo apt-get install -y tshark
 else
     echo "tshark is already installed"
 fi
