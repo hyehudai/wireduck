89c49a4a6c88f27c41d219c8141c2e476d4fe35e
hyehudai <h.yehudai@gmail.com>
2025-03-29 19:15:32 +0300

    pull wireshark

--- tools/wireduck_configure_ci_patch.sh
+++ tools/wireduck_configure_ci_patch.sh
@@ -2,19 +2,7 @@
 set -eux
 
 # Define version and paths
-TSHARK_DEB="tshark_4.2.6-1_amd64.deb"
-TSHARK_URL="http://archive.ubuntu.com/ubuntu/pool/universe/w/wireshark/$TSHARK_DEB"
-TSHARK_DIR="$PWD/tools/tshark"
-
-# Create target dir
-mkdir -p "$TSHARK_DIR"
-cd "$TSHARK_DIR"
-pwd
-# Download and extract
-wget -N "$TSHARK_URL"
-dpkg -x "$TSHARK_DEB" .
-$TSHARK_DIR/usr/bin/tshark -v
-
+docker pull wireshark/tshark
 # Add alias to your shell temporarily (for current session)
 #echo "alias tshark='$TSHARK_DIR/bin/tshark'"
 alias tsharp="$TSHARK_DIR/usr/bin/tshark"
